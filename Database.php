ab11eb45faa22043534971cb9bb29a58d8e69818
bairuden <129077359+bairuden@users.noreply.github.com>
2024-09-27 18:40:41 +0600

    first commit

---
+++ Database.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = " ";
+$dbname = a_phptute;
+$conn =  new php_connect($hostname,$username,$password,$dbname);
+if(!connect){
+
+
+echo "connection failed";
+}
+?>
